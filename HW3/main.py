6a3bb27026ab52c16c4ed71e71ffa69df69950d4
samuelharroch <samuel@optival.com>
2022-04-06 01:49:13 +0300

    remove main

--- HW3/main.py
+++
@@ -1,9 +0,0 @@
-from items import Item, ItemWithName
-if __name__ == '__main__':
-    item1 = Item(1, 2)
-    item2 = ItemWithName('gold', 1, 2)
-
-    print(item1)
-    print(item2)
-    a= [item1, item2]
-    print(a)
